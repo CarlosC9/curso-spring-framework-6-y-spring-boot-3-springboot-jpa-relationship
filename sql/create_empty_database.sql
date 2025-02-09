22c08406ae56cfd2f8dc193a71428e2bd9de983f
Carlos <carlosicaraballo1999@gmail.com>
2025-02-09 16:47:38 +0000

    155. Configurando la conexión en el properties

---
+++ sql/create_empty_database.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS db_jpa_relationship;
+CREATE DATABASE db_jpa_relationship;
+
+USE db_jpa_relationship;
+
+CREATE TABLE clients
+(
+    id       bigint not null auto_increment,
+    name     varchar(255),
+    lastname varchar(255),
+    primary key (id)
+);
+
+CREATE TABLE invoices
+(
+    id          bigint not null auto_increment,
+    description varchar(255),
+    total       bigint,
+    primary key (id)
+);
+
+ALTER TABLE invoices
+    ADD column client bigint,
+    ADD FOREIGN KEY (client) REFERENCES clients (id) ON DELETE SET NULL;
---
+++ sql/fill_database_with_dev_data.sql
@@ -0,0 +1,4 @@
+insert into clients (name, lastname)
+values ('Pepe', 'Doe');
+insert into clients (name, lastname)
+values ('Maria', 'Roe');
