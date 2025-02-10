dfdcd41b5d617d8f2bccb6eeea854c9df9bfce14
Carlos <carlosicaraballo1999@gmail.com>
2025-02-10 14:32:50 +0000

    157. Codificando ejemplo ManyToOne con relación existente

--- src/main/java/com/carlos/curso/springboot/jpa/springbootjparelationship/SpringbootJpaRelationshipApplication.java
+++ src/main/java/com/carlos/curso/springboot/jpa/springbootjparelationship/SpringbootJpaRelationshipApplication.java
@@ -11,40 +11,56 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @SpringBootApplication
 public class SpringbootJpaRelationshipApplication implements CommandLineRunner {
 
-    private ClientRepository clientRepository;
+  private ClientRepository clientRepository;
 
-    private InvoiceRepository invoiceRepository;
+  private InvoiceRepository invoiceRepository;
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringbootJpaRelationshipApplication.class, args);
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootJpaRelationshipApplication.class, args);
+  }
+
+  @Transactional
+  public void manyToOne() {
+    Client client = new Client("John", "Doe");
+    this.clientRepository.save(client);
+    Invoice invoice = new Invoice("compras de oficina", 2000L);
+    invoice.setClient(client);
+    Invoice invoiceDb = this.invoiceRepository.save(invoice);
+    System.out.println(invoiceDb);
+
+  }
+
+  @Transactional
+  public void manyToOneFindById() {
+    Optional<Client> optionalClient = this.clientRepository.findById(1L);
+
+    if (optionalClient.isPresent()) {
+      Client client = optionalClient.orElseThrow();
+      Invoice invoice = new Invoice("manyToOneFindById", 2000L);
+      invoice.setClient(client);
+      Invoice invoiceDb = this.invoiceRepository.save(invoice);
+      System.out.println(invoiceDb);
     }
+  }
 
-    @Transactional
-    public void manyToOne() {
-        Client client = new Client("John", "Doe");
-        this.clientRepository.save(client);
-        Invoice invoice = new Invoice("compras de oficina", 2000L);
-        invoice.setClient(client);
-        Invoice invoiceDb = this.invoiceRepository.save(invoice);
-        System.out.println(invoiceDb);
+  @Override
+  public void run(String... args) throws Exception {
+//        this.manyToOne();
+    this.manyToOneFindById();
+  }
 
-    }
+  @Autowired
+  public void setClientRepository(ClientRepository clientRepository) {
+    this.clientRepository = clientRepository;
+  }
 
-    @Override
-    public void run(String... args) throws Exception {
-        this.manyToOne();
-    }
-
-    @Autowired
-    public void setClientRepository(ClientRepository clientRepository) {
-        this.clientRepository = clientRepository;
-    }
-
-    @Autowired
-    public void setInvoiceRepository(InvoiceRepository invoiceRepository) {
-        this.invoiceRepository = invoiceRepository;
-    }
+  @Autowired
+  public void setInvoiceRepository(InvoiceRepository invoiceRepository) {
+    this.invoiceRepository = invoiceRepository;
+  }
 }
